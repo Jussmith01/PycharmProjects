2121fdb3113471fe4e24b321d9ab62245cc5cf2f
Justin <jussmith48@gmail.com>
2015-08-30 20:21:02 -0400

    Initial Commit

---
+++ graphs.py/graphs.py
@@ -0,0 +1,38 @@
+__author__ = 'dustintracy'
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+# Open and read the data file
+#infile = open('/home/jujuman/Gits/ForcePredictionNetwork/g09DNNTSData/TestCase6/RawData2/graph_expvact.dat', 'r')
+infile = open('/home/jujuman/Gits/ForcePredictionNetwork/g09DNNTSData/UNIH22.5Network/GPU0/trainingDataz.dat', 'r')
+
+data = []
+
+for line in infile:
+    row = line.strip().split(",")
+    data.append(row)
+
+# Truncate and convert to numpy array
+data_array = np.array(data)
+
+data = data_array[:-1, [0, 2]]
+data = np.array(data, dtype=float)
+#data = abs(data)
+#data_n = data[:, [0, 1]]
+#data_n[:, 1] = data[:, 2] - data[:, 1]
+
+d01 = data[:, 0]
+d02 = data[:, 1]
+#f1 = data[:, 2]
+
+#fig = plt.figure()
+#ax = fig.add_subplot(111, projection='2d')
+#ax.scatter(d01,d02)
+#ax.set_xlabel("d01")
+#ax.set_ylabel("d02")
+#ax.set_zlabel("f1")
+plt.scatter(d01,d02)
+plt.xlabel("d01")
+plt.ylabel("d02")
+plt.show()
