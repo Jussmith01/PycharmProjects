92f06a3f1d6cffae6e238cb1e2ba734d74f4a685
Justin Smith <jussmith48@gmail.com>
2016-02-07 13:45:55 -0500

    Function grapher

--- H2Graphing/functiongrapher.py
+++ H2Graphing/functiongrapher.py
@@ -19,22 +19,112 @@ import numpy as np
 #plt.plot(dE, P)
 
 #plt.show()
+#--------------------------------
+#           Functions
+#--------------------------------
 
 # ------------------------------------------
-# Calculate the RMSD given two structures
+#               Radial Function
 # ------------------------------------------
-def computedataset(x1,x2,pts,eta,Rc,plt,scolor,slabel):
+def radialfunction(X,eta,Rc):
+    F = np.exp(-eta*(X-0.25)**2.0) * (0.5 * (np.cos((np.pi * X)/Rc) + 1.0))
+    return F
+
+# ------------------------------------------
+#               Angular Function
+# ------------------------------------------
+def angularfunction(T,zeta,lam):
+    F = (2.0**(1.0-zeta)) * ((1.0 + lam * np.cos(T))**zeta)
+    return F
+
+
+# ------------------------------------------
+# Calculate The Steps for a Radial Dataset
+# ------------------------------------------
+def computeradialdataset(x1,x2,pts,eta,Rc,plt,scolor,slabel):
 
     X = np.linspace(x1, x2, pts, endpoint=True)
-    F = np.exp(-eta*(X-0.25)**2.0) * (0.25 * (np.cos((np.pi * X)/Rc) + 1.0))
+    F = radialfunction(X,eta,Rc)
     plt.plot(X, F, label=slabel, color=scolor)
 
+# ------------------------------------------
+# Calculate The Steps for an angular Dataset
+# ------------------------------------------
+def computeangulardataset(t1,t2,pts,zeta,lam,plt,scolor,slabel):
 
-computedataset(0.0, 10.0, 1000, 0.01, 6.0, plt, 'blue', 'eta = 0.1')
-computedataset(0.0, 10.0, 1000, 0.05, 6.0, plt, 'green', 'eta = 0.1')
-computedataset(0.0, 10.0, 1000, 0.10, 6.0, plt, 'red', 'eta = 0.2')
-computedataset(0.0, 10.0, 1000, 0.30, 6.0, plt, 'orange', 'eta = 0.3')
-computedataset(0.0, 10.0, 1000, 0.40, 6.0, plt, 'yellow', 'eta = 0.4')
-computedataset(0.0, 10.0, 1000, 5.0, 6.0, plt, 'black', 'eta = 0.5')
+    T = np.linspace(t1, t2, pts, endpoint=True)
+    F = angularfunction(T,zeta,lam)
+    plt.plot(T, F, label=slabel, color=scolor)
+
+# ------------------------------------------
+# Calculate The Steps for an angular Dataset
+# ------------------------------------------
+def printdatatofile(f,title,X,N):
+    f.write(title + ' = [')
+    for i in range(0,N):
+        if i < N-1:
+            s = "{:.7e}".format(X[i]) + ','
+        else:
+            s = "{:.7e}".format(X[i])
+        f.write(s)
+    f.write(']\n')
+
+#--------------------------------
+#          Parameters
+#--------------------------------
+#File name
+pf = 'test.params'
+
+Nrr = 32
+Nat = 4
+Nar = 8
+Nzt = 8
+
+Rc = 6.0
+Atyp = '[H,C,O]'
+
+
+#--------------------------------
+#           Program
+#--------------------------------
+Nrt = Nrr * Nat
+EtaR = np.zeros(Nrr)
+for i in range(0,Nrr):
+    stddev = float(Nrr)/4.5
+    step = 3.5 * np.exp(-(float(i-0.0)**2)/(2.0*(stddev)**2.0))
+    computeradialdataset(0.25, Rc, 1000, step, 6.0, plt, 'blue', 'eta = 0.1')
+    EtaR[i] = step
 
 plt.show()
+
+EtaA = np.zeros(Nar)
+Zeta = np.zeros(Nzt)
+
+Nat = Nar * Nzt
+for i in range(0,Nzt):
+    step = float(i)
+    computeangulardataset(0.0,2.0*np.pi,1000,step,1.0,plt, 'red', 'eta = 0.1')
+    Zeta[i] = step
+
+plt.show()
+
+for i in range(0,Nar):
+    stddev = float(Nar)/5.5
+    step = 3.5 * np.exp(-(float(i-0.0)**2)/(2.0*(stddev)**2.0))
+    computeradialdataset(0.25, Rc, 1000, step, 6.0, plt, 'blue', 'eta = 0.1')
+    EtaA[i] = step
+
+plt.show()
+
+Nt = Nrt + Nat
+print('Total N Size: ',Nt)
+
+# Open File
+f = open(pf,'w')
+
+#Write data to parameters file
+f.write('Rc = ' + "{:.7e}".format(Rc) + '\n')
+printdatatofile(f,'EtaR',EtaR,Nrr)
+printdatatofile(f,'EtaA',EtaA,Nar)
+printdatatofile(f,'Zeta',Zeta,Nzt)
+f.write('Atyp = ' + Atyp + '\n')
