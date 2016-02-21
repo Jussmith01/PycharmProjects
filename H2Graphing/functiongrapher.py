8df97f4f8d9b4683d0b2320e8a70881c0fefa092
Justin Smith <jussmith48@gmail.com>
2016-02-20 23:44:08 -0500

    new sfl param builfer ideas

--- H2Graphing/functiongrapher.py
+++ H2Graphing/functiongrapher.py
@@ -26,34 +26,34 @@ import numpy as np
 # ------------------------------------------
 #               Radial Function
 # ------------------------------------------
-def radialfunction(X,eta,Rc):
-    F = np.exp(-eta*(X-0.25)**2.0) * (0.5 * (np.cos((np.pi * X)/Rc) + 1.0))
+def radialfunction(X,eta,Rc,Rs):
+    F = np.exp(-eta*(X-Rs)**2.0) * (0.5 * (np.cos((np.pi * X)/Rc) + 1.0))
     return F
 
 # ------------------------------------------
 #               Angular Function
 # ------------------------------------------
-def angularfunction(T,zeta,lam):
-    F = (2.0**(1.0-zeta)) * ((1.0 + lam * np.cos(T))**zeta)
+def angularfunction(T,zeta,lam,Ts):
+    F = (2.0**(1.0-zeta)) * ((1.0 + lam * np.cos(T-Ts))**zeta)
     return F
 
 
 # ------------------------------------------
 # Calculate The Steps for a Radial Dataset
 # ------------------------------------------
-def computeradialdataset(x1,x2,pts,eta,Rc,plt,scolor,slabel):
+def computeradialdataset(x1,x2,pts,eta,Rc,Rs,plt,scolor,slabel):
 
     X = np.linspace(x1, x2, pts, endpoint=True)
-    F = radialfunction(X,eta,Rc)
+    F = radialfunction(X,eta,Rc,Rs)
     plt.plot(X, F, label=slabel, color=scolor)
 
 # ------------------------------------------
 # Calculate The Steps for an angular Dataset
 # ------------------------------------------
-def computeangulardataset(t1,t2,pts,zeta,lam,plt,scolor,slabel):
+def computeangulardataset(t1,t2,pts,zeta,lam,Ts,plt,scolor,slabel):
 
     T = np.linspace(t1, t2, pts, endpoint=True)
-    F = angularfunction(T,zeta,lam)
+    F = angularfunction(T,zeta,lam,Ts)
     plt.plot(T, F, label=slabel, color=scolor)
 
 # ------------------------------------------
@@ -72,12 +72,12 @@ def printdatatofile(f,title,X,N):
 #--------------------------------
 #          Parameters
 #--------------------------------
-#File name
+#File nam
 pf = 'test.params' # Output filename
 
-Nrr = 12
+Nrr = 16
 Nat = 3
-Nar = 6
+Nar = 8
 Nzt = 10
 
 Rc = 6.0
@@ -91,8 +91,13 @@ EtaR = np.zeros(Nrr)
 for i in range(0,Nrr):
     stddev = float(Nrr)/4.5
     step = 3.5 * np.exp(-(float(i-0.0)**2)/(2.0*(stddev)**2.0))
-    computeradialdataset(0.25, Rc, 1000, step, 6.0, plt, 'blue', 'eta = 0.1')
+    computeradialdataset(0.5, Rc, 1000, step, Rc,0.25, plt, 'blue', 'eta = 0.1')
     EtaR[i] = step
+
+plt.show()
+
+for i in range(0,Nrr):
+    computeradialdataset(0.5, Rc, 1000, 3.5, Rc,(i**1.5)*0.1+0.25, plt, 'blue', 'eta = 0.1')
 
 plt.show()
 
@@ -102,18 +107,23 @@ Zeta = np.zeros(Nzt)
 Nat = Nar * Nzt
 for i in range(0,Nzt):
     step = float(i+1.0)
-    computeangulardataset(0.0,2.0*np.pi,1000,step,1.0,plt, 'red', 'eta = 0.1')
+    computeangulardataset(0.0,2.0*np.pi,1000,step,1.0,0.0,plt, 'red', 'eta = 0.1')
     Zeta[i] = step
+
+plt.show()
+
+for i in range(0,Nzt):
+    computeangulardataset(0.0,2.0*np.pi,1000,16,-1.0,i*0.63,plt, 'red', 'eta = 0.1')
 
 plt.show()
 
 for i in range(0,Nar):
     stddev = float(Nar)/4.5
     step = 3.5 * np.exp(-(float(i-0.0)**2)/(2.0*(stddev)**2.0))
-    computeradialdataset(0.25, Rc, 1000, step, 6.0, plt, 'blue', 'eta = 0.1')
+    computeradialdataset(0.25, Rc, 1000, step, 6.0,0.25, plt, 'blue', 'eta = 0.1')
     EtaA[i] = step
 
-plt.show()
+#plt.show()
 
 Nt = Nrt + Nat
 print('Total N Size: ',Nt)
