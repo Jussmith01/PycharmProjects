9731b3c53cc578408efd3b81f3eaec084c971c01
Justin Smith <jussmith48@gmail.com>
2016-02-21 20:28:17 -0500

    Modded symfunc input producer

--- H2Graphing/functiongrapher.py
+++ H2Graphing/functiongrapher.py
@@ -77,53 +77,76 @@ pf = 'test.params' # Output filename
 
 Nrr = 16
 Nat = 3
-Nar = 8
-Nzt = 10
+Nar = 5
+Nzt = 15
 
 Rc = 6.0
 Atyp = '[H,C,O]'
+EtaR = 20.0
+EtaA = 4.0
+Zeta = 32.0
 
 #--------------------------------
 #           Program
 #--------------------------------
 Nrt = Nrr * Nat
-EtaR = np.zeros(Nrr)
-for i in range(0,Nrr):
-    stddev = float(Nrr)/4.5
-    step = 3.5 * np.exp(-(float(i-0.0)**2)/(2.0*(stddev)**2.0))
-    computeradialdataset(0.5, Rc, 1000, step, Rc,0.25, plt, 'blue', 'eta = 0.1')
-    EtaR[i] = step
+ShfR = np.zeros(Nrr)
 
-plt.show()
-
-for i in range(0,Nrr):
-    computeradialdataset(0.5, Rc, 1000, 3.5, Rc,(i**1.5)*0.1+0.25, plt, 'blue', 'eta = 0.1')
-
-plt.show()
-
-EtaA = np.zeros(Nar)
-Zeta = np.zeros(Nzt)
-
-Nat = Nar * Nzt
-for i in range(0,Nzt):
-    step = float(i+1.0)
-    computeangulardataset(0.0,2.0*np.pi,1000,step,1.0,0.0,plt, 'red', 'eta = 0.1')
-    Zeta[i] = step
-
-plt.show()
-
-for i in range(0,Nzt):
-    computeangulardataset(0.0,2.0*np.pi,1000,16,-1.0,i*0.63,plt, 'red', 'eta = 0.1')
-
-plt.show()
-
-for i in range(0,Nar):
-    stddev = float(Nar)/4.5
-    step = 3.5 * np.exp(-(float(i-0.0)**2)/(2.0*(stddev)**2.0))
-    computeradialdataset(0.25, Rc, 1000, step, 6.0,0.25, plt, 'blue', 'eta = 0.1')
-    EtaA[i] = step
+#for i in range(0,Nrr):
+#    stddev = float(Nrr)/4.5
+#    step = 3.5 * np.exp(-(float(i-0.0)**2)/(2.0*(stddev)**2.0))
+#    computeradialdataset(0.5, Rc, 1000, step, Rc,0.25, plt, 'blue', 'eta = 0.1')
+#    EtaR[i] = step
 
 #plt.show()
+
+#Now instead of multiple etaR we use multiple shifts with a single large EtaR
+for i in range(0,Nrr):
+    stepsize = Rc / float(Nrr+1.0)
+    #step = (i**1.5)*0.1+0.5
+    step = i * stepsize + 0.5
+    computeradialdataset(0.5, Rc, 1000, 20.0, Rc,step, plt, 'blue', 'eta = '+str(EtaR))
+    ShfR[i] = step
+
+plt.show()
+
+ShfZ = np.zeros(Nzt)
+
+Nat = Nar * Nzt
+
+#for i in range(0,Nzt):
+#    step = float(i+1.0)
+#    computeangulardataset(0.0,2.0*np.pi,1000,step,1.0,0.0,plt, 'red', 'eta = 0.1')
+#    Zeta[i] = step
+
+#plt.show()
+
+for i in range(0,Nzt):
+    stepsize = (2.0 * np.pi) / (float(Nzt))
+    step = i*stepsize
+    computeangulardataset(0.0,2.0*np.pi,1000,Zeta,1.0,step,plt, 'red', 'zeta = ' + str(Zeta))
+    ShfZ[i] = step
+
+plt.show()
+
+#for i in range(0,Nar):
+#    stddev = float(Nar)/4.5
+#    step = 3.5 * np.exp(-(float(i-0.0)**2)/(2.0*(stddev)**2.0))
+#    computeradialdataset(0.25, Rc, 1000, step, 6.0,0.25, plt, 'blue', 'eta = 0.1')
+#    EtaA[i] = step
+
+#plt.show()
+
+ShfA = np.zeros(Nar)
+
+for i in range(0,Nar):
+    stepsize = Rc / float(Nar+1.0)
+    #step = (i**1.5)*0.1+0.5
+    step = i * stepsize + 0.5
+    computeradialdataset(0.5, Rc, 1000, EtaA, Rc,step, plt, 'blue', 'eta = '+str(EtaR))
+    ShfA[i] = step
+
+plt.show()
 
 Nt = Nrt + Nat
 print('Total N Size: ',Nt)
@@ -133,7 +156,12 @@ f = open(pf,'w')
 
 #Write data to parameters file
 f.write('Rc = ' + "{:.7e}".format(Rc) + '\n')
-printdatatofile(f,'EtaR',EtaR,Nrr)
-printdatatofile(f,'EtaA',EtaA,Nar)
-printdatatofile(f,'Zeta',Zeta,Nzt)
+f.write('EtaR = ' + "{:.7e}".format(EtaR) + '\n')
+printdatatofile(f,'ShfR',ShfR,Nrr)
+f.write('Zeta = ' + "{:.7e}".format(Zeta) + '\n')
+printdatatofile(f,'ShfZ',ShfZ,Nzt)
+f.write('EtaA = ' + "{:.7e}".format(EtaA) + '\n')
+printdatatofile(f,'ShfA',ShfA,Nar)
 f.write('Atyp = ' + Atyp + '\n')
+
+f.close()
