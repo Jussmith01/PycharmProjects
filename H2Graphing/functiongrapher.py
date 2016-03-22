fc5925d3ad6a3c35f62ebf22b38db083304aadbd
Justin <jsmith48@ufl.edu>
2016-03-21 21:57:13 -0400

    Update

--- H2Graphing/functiongrapher.py
+++ H2Graphing/functiongrapher.py
@@ -12,10 +12,17 @@ import scipy.interpolate
 #--------------------------------
 
 # ------------------------------------------
-#               Radial Function
+#          Radial Function Cos w/ Sqrt
 # ------------------------------------------
-def radialfunction(X,eta,Rc,Rs):
-    F = np.sqrt(np.exp(-eta*(X-Rs)**2.0) * (0.5 * (np.cos((np.pi * X)/Rc) + 1.0)))
+def radialfunctionsqrt(X,eta,Rc,Rs):
+    F = np.exp(-eta*(X-Rs)**2.0) * (0.5 * (np.cos((np.pi * X)/Rc) + 1.0))
+    return F
+
+# ------------------------------------------
+#          Radial Function Cos
+# ------------------------------------------
+def radialfunctioncos(X,eta,Rc,Rs):
+    F = np.exp(-eta*(X-Rs)**2.0) * (0.5 * (np.cos((np.pi * X)/Rc) + 1.0))
     return F
 
 # ------------------------------------------
@@ -49,7 +56,7 @@ def angularfunction(T,zeta,lam,Ts):
 def computeradialdataset(x1,x2,pts,eta,Rc,Rs,plt,scolor,slabel):
 
     X = np.linspace(x1, x2, pts, endpoint=True)
-    F = radialfunction(X,eta,Rc,Rs)
+    F = radialfunctionsqrt(X,eta,Rc,Rs)
     plt.plot(X, F, label=slabel, color=scolor, linewidth=2)
 
 # ------------------------------------------
@@ -83,7 +90,10 @@ def printdatatofile(f,title,X,N):
         f.write(s)
     f.write(']\n')
 
-def show2dcontangulargraph (eta,zeta,Rs,Rc,M):
+def add (x,y):
+    return x+y
+
+def show2dcontangulargraph (ShfA,ShfZ,eta,zeta,Rc,func):
     N = 1000000
     x, y = 12.0 * np.random.random((2, N)) - 6.0
 
@@ -92,17 +102,51 @@ def show2dcontangulargraph (eta,zeta,Rs,Rc,M):
     R = np.sqrt(x**2 + y**2)
     T = np.arctan2(x,y)
 
-    z = angularfunction(T,zeta,1.0,0.0) * radialfunction(R,eta,Rc,Rs)**2
+    z = np.zeros(N)
 
-    for i in range(1,M):
-        Ts = float(i) * (2.0*np.pi/float(M))
-        print(Ts)
-        if Ts < np.pi:
-            zt = angularfunction(T,zeta,1.0,Ts) * radialfunction(R,eta,Rc,Rs)**2
-        else:
-            zt = angularfunction(T,zeta,1.0,Ts) * radialfunction2(R,eta,Rc,Rs)**2
+    for i in ShfZ:
+        #Ts = float(i) * (2.0*np.pi/float(M))
+        #print(Ts)
+        #if Ts < np.pi:
+        #    zt = angularfunction(T,zeta,1.0,Ts) * radialfunction(R,eta,Rc,Rs)**2
+        #else:
 
-        z = z + zt
+        for j in ShfA:
+            print( 'ShfZ: ' + str(i) + ' ShfA: ' + str(j) )
+            zt = angularfunction(T,zeta,1.0,i) * radialfunctionsqrt(R,eta,Rc,j) * radialfunctionsqrt(R,eta,Rc,j)
+
+            for k in range(1,z.shape[0]):
+                z[k] = func(z[k],zt[k])
+
+    # Set up a regular grid of interpolation points
+    xi, yi = np.linspace(x.min(), y.max(), 300), np.linspace(x.min(), y.max(), 300)
+    xi, yi = np.meshgrid(xi, yi)
+
+    zi = scipy.interpolate.griddata((x, y), z, (xi, yi), method='linear')
+
+    plt.imshow(zi, vmin=z.min(), vmax=z.max(), origin='lower',
+           extent=[x.min(), x.max(), y.min(), y.max()])
+
+    plt.colorbar()
+    plt.show()
+
+def show2dcontradialgraph (ShfR,eta,Rc,func):
+    N = 1000000
+    x, y = 12.0 * np.random.random((2, N)) - 6.0
+
+    print(x)
+
+    R = np.sqrt(x**2 + y**2)
+    T = np.arctan2(x,y)
+
+    z = np.zeros(N)
+
+    for j in ShfR:
+        print( 'ShfZ: ' + str(i) + ' ShfA: ' + str(j) )
+        zt = radialfunctioncos(R,eta,Rc,j)
+
+        for k in range(1,z.shape[0]):
+            z[k] = func(z[k],zt[k])
 
     # Set up a regular grid of interpolation points
     xi, yi = np.linspace(x.min(), y.max(), 300), np.linspace(x.min(), y.max(), 300)
@@ -120,19 +164,18 @@ def show2dcontangulargraph (eta,zeta,Rs,Rc,M):
 #          Parameters
 #--------------------------------
 #File nam
-pf = 'rHCNO-24-a1-4.params' # Output filename
+pf = 'rHCNO-11-a6-10.params' # Output filename
 
-Nrr = 24
+Nrr = 11
 Na = 4
-Nar = 1
-Nzt = 2
+Nar = 6
+Nzt = 10
 
 Rc = 6.0
 Atyp = '[H,C,N,O]'
-EtaR = 4.0
-EtaA1 = 0.1
-EtaA2 = 0.5
-Zeta = 64.0
+EtaR = 8.0
+EtaA1 = 4.0
+Zeta = 16.0
 
 #--------------------------------
 #           Program
@@ -152,10 +195,9 @@ ShfR = np.zeros(Nrr)
 for i in range(0,Nrr):
     stepsize = Rc / float(Nrr+1.0)
     #step = (i**1.5)*0.1+0.5
-    step = i * stepsize + 0.25
+    step = i * stepsize + 0.50
     computeradialdataset(0.5, Rc, 1000, EtaR, Rc,step, plt, 'blue', 'eta = '+ str(EtaR))
     ShfR[i] = step
-
 
 plt.title('Radial Environment Functions (REF)')
 #plt.title('SCAN: Formic Acid Energy vs. H-O-H Angle')
@@ -163,9 +205,12 @@ plt.ylabel('REF Output')
 plt.xlabel('Angstroms')
 plt.show()
 
+#show2dcontradialgraph(ShfR,EtaR,Rc,add)
+#show2dcontradialgraph(ShfR,EtaR,Rc,max)
+
 ShfZ = np.zeros(Nzt)
 
-Nat = Nar * Nzt
+Nat = (Nar*(Nar+1)/2) * Nzt
 
 #for i in range(0,Nzt):
 #    step = float(i+1.0)
@@ -196,15 +241,17 @@ for i in range(0,Nar):
     stepsize = Rc / float(Nar+1.0)
     #step = (i**1.5)*0.1+0.5
     step = i * stepsize + 0.5
-    computeradial2dataset(0.5, Rc, 1000, EtaA2, Rc,step, plt, 'blue', 'eta = '+ str(EtaA2))
+    #computeradial2dataset(0.5, Rc, 1000, EtaA2, Rc,step, plt, 'blue', 'eta = '+ str(EtaA2))
     computeradialdataset(0.5, Rc, 1000, EtaA1, Rc,step, plt, 'blue', 'eta = '+ str(EtaA1))
     ShfA[i] = step
 
 plt.show()
 
-show2dcontangulargraph(EtaA2,Zeta,0.5,Rc,Nzt)
+show2dcontangulargraph(ShfA,ShfZ,EtaA1,Zeta,Rc,add)
+show2dcontangulargraph(ShfA,ShfZ,EtaA1,Zeta,Rc,max)
 
-Nt = Nat * (Na*(Na+1)/2) + Nrt
+#Nt = Nat * (Na*(Na+1)/2) + Nrt
+Nt = Nat + Nrt
 print('Total N Size: ',int(Nt))
 
 # Open File
@@ -216,7 +263,7 @@ f.write('EtaR = ' + "{:.4}".format(EtaR) + '\n')
 printdatatofile(f,'ShfR',ShfR,Nrr)
 f.write('Zeta = ' + "{:.4}".format(Zeta) + '\n')
 printdatatofile(f,'ShfZ',ShfZ,Nzt)
-f.write('EtaA = ' + "{:.4}".format(EtaA2) + '\n')
+f.write('EtaA = ' + "{:.4}".format(EtaA1) + '\n')
 printdatatofile(f,'ShfA',ShfA,Nar)
 f.write('Atyp = ' + Atyp + '\n')
 
