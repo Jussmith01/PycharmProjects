9132f085e5625d3b3b5092384f6d477d5431bd60
Justin <jussmith48@gmail.com>
2016-04-19 14:58:06 -0400

    Update

--- H2Graphing/functiongrapher.py
+++ H2Graphing/functiongrapher.py
@@ -187,19 +187,19 @@ def show2dcontradialgraph (ShfR,eta,Rc,func,title):
 #         Set Parameters
 #--------------------------------
 #File nam
-pf = 'rHCNO-16-a8-8.params' # Output filename
+pf = 'rHCNO-6-a4-4.params' # Output filename
 
-Nrr = 16
+Nrr = 6
 Na = 4
-Nar = 8
-Nzt = 8
+Nar = 4
+Nzt = 4
 
 TM = 1
-Rc = 6.0
+Rc = 3.0
 Atyp = '[H,C,O,N]'
-EtaR = np.array([16.0])
-EtaA = np.array([6.0])
-Zeta = np.array([20.0])
+EtaR = np.array([4.0])
+EtaA = np.array([4.0])
+Zeta = np.array([5.0])
 
 # ****************************************************
 cmap = mpl.cm.brg
