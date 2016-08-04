2f0c329e1e861433ba0578539313c09d6c85616d
Justin <jussmith48@gmail.com>
2016-08-04 07:10:42 -0400

    Update

--- HD-AtomNNP/EdiffGraph.py
+++ HD-AtomNNP/EdiffGraph.py
@@ -16,22 +16,26 @@ cmap = mpl.cm.brg
 # ------------
 user = os.environ['USER']
 
-dir = '/Research/ANN-Test-Data/GDB-11-W98XD-6-31gd/train_05/'
-dir2 = '/Research/ANN-Test-Data/GDB-11-W98XD-6-31gd/train_06/'
+dir = '/Research/ANN-Test-Data/GDB-11-W98XD-6-31gd/train_04/'
+dir2 = '/Research/ANN-Test-Data/GDB-11-W98XD-6-31gd/train_05/'
+dir3 = '/Research/ANN-Test-Data/GDB-11-W98XD-6-31gd/train_06/'
 
-file = 'gdb11_s05-0_test.dat_graph'
+file = 'polypep_test.dat_graph'
 
 data1 = gt.getfltsfromfile('/home/' + user + dir + file, [0])
 data2 = gt.getfltsfromfile('/home/' + user + dir + file, [1])
 data3 = gt.getfltsfromfile('/home/' + user + dir + file, [2])
 data4 = gt.getfltsfromfile('/home/' + user + dir2 + file, [2])
+data5 = gt.getfltsfromfile('/home/' + user + dir3 + file, [2])
 
 data2 = gt.calculateelementdiff(data2)
 data3 = gt.calculateelementdiff(data3)
 data4 = gt.calculateelementdiff(data4)
+data5 = gt.calculateelementdiff(data5)
 
-rmse5 = gt.calculaterootmeansqrerror(data2[:,1],data3[:,1]) / 16.0
-rmse6 = gt.calculaterootmeansqrerror(data2[:,1],data4[:,1]) / 16.0
+rmse4 = gt.calculaterootmeansqrerror(data2[:,1],data3[:,1]) / 63.0
+rmse5 = gt.calculaterootmeansqrerror(data2[:,1],data4[:,1]) / 63.0
+rmse6 = gt.calculaterootmeansqrerror(data2[:,1],data5[:,1]) / 63.0
 
 print('Datasize: ' + str(data1.shape[0]))
 
@@ -43,10 +47,12 @@ plt.rc('font', **font)
 
 plt.plot(data2[:,0], data2[:,1], color='black', label='wB97X/6-31G*',linewidth=2)
 plt.scatter(data2[:,0], data2[:,1], color='black',linewidth=4)
-plt.plot(data3[:,0], data3[:,1],'r--', color='blue', label='ANN - GDB-5 RMSE: ' + "{:.6f}".format(rmse5) + "Ha/atom",linewidth=2)
+plt.plot(data3[:,0], data3[:,1],'r--', color='blue', label='ANN - GDB-4 RMSE: ' + "{:.6f}".format(rmse4) + "Ha/atom",linewidth=2)
 plt.scatter(data3[:,0], data3[:,1], color='blue',linewidth=4)
-plt.plot(data4[:,0], data4[:,1],'r--', color='red', label='ANN - GDB-6 RMSE: ' + "{:.6f}".format(rmse6) + "Ha/atom",linewidth=2)
-plt.scatter(data4[:,0], data4[:,1], color='red',linewidth=4)
+plt.plot(data4[:,0], data4[:,1],'r--', color='green', label='ANN - GDB-5 RMSE: ' + "{:.6f}".format(rmse5) + "Ha/atom",linewidth=2)
+plt.scatter(data4[:,0], data4[:,1], color='green',linewidth=4)
+plt.plot(data5[:,0], data5[:,1],'r--', color='red', label='ANN - GDB-6 RMSE: ' + "{:.6f}".format(rmse6) + "Ha/atom",linewidth=2)
+plt.scatter(data5[:,0], data5[:,1], color='red',linewidth=4)
 
 plt.title("Energy Differences Between 50 Random Structures\nPolypeptide Chain: H-Gly-Pro-Hyp-Gly-Ala-Gly-OH")
 plt.xlabel('Conformation Pair (Count 49)')
