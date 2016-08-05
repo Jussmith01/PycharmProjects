166576eeb0145fb0dfd97dcc3c74c977a733446b
Justin <jsmith48@ufl.edu>
2016-08-04 21:33:22 -0400

    Update

--- HD-AtomNNP/EdiffGraph.py
+++ HD-AtomNNP/EdiffGraph.py
@@ -9,6 +9,7 @@ import matplotlib as mpl
 import random
 import graphtools as gt
 
+
 # -----------------------
 cmap = mpl.cm.brg
 # ------------
@@ -16,14 +17,15 @@ cmap = mpl.cm.brg
 # ------------
 user = os.environ['USER']
 
-dir1 = '/Research/ANN-Test-Data/GDB-11-W98XD-6-31gd/train_04/'
-dir2 = '/Research/ANN-Test-Data/GDB-11-W98XD-6-31gd/train_05/'
-dir3 = '/Research/ANN-Test-Data/GDB-11-W98XD-6-31gd/train_06/'
+dir1 = '/Research/ANN-Test-Data/GDB-11-W98XD-6-31gd/train_05/'
+dir2 = '/Research/ANN-Test-Data/GDB-11-W98XD-6-31gd/train_06/'
+dir3 = '/Research/ANN-Test-Data/GDB-11-W98XD-6-31gd/train_06_2/'
 dir4 = '/Research/ANN-Test-Data/GDB-11-W98XD-6-31gd/train_06AA/'
 
 file = 'polypep_test.dat_graph'
 
 data1 = gt.getfltsfromfile('/home/' + user + dir1 + file, [0])
+
 data2 = gt.getfltsfromfile('/home/' + user + dir1 + file, [1])
 data3 = gt.getfltsfromfile('/home/' + user + dir1 + file, [2])
 data4 = gt.getfltsfromfile('/home/' + user + dir2 + file, [2])
@@ -36,9 +38,9 @@ data4 = gt.calculateelementdiff(data4)
 data5 = gt.calculateelementdiff(data5)
 data6 = gt.calculateelementdiff(data6)
 
-rmse4 = gt.calculaterootmeansqrerror(data2[:,1],data3[:,1]) / 63.0
-rmse5 = gt.calculaterootmeansqrerror(data2[:,1],data4[:,1]) / 63.0
-rmse6 = gt.calculaterootmeansqrerror(data2[:,1],data5[:,1]) / 63.0
+rmse5 = gt.calculaterootmeansqrerror(data2[:,1],data3[:,1]) / 63.0
+rmse61 = gt.calculaterootmeansqrerror(data2[:,1],data4[:,1]) / 63.0
+rmse62 = gt.calculaterootmeansqrerror(data2[:,1],data5[:,1]) / 63.0
 rmse6AA = gt.calculaterootmeansqrerror(data2[:,1],data6[:,1]) / 63.0
 
 print('Datasize: ' + str(data1.shape[0]))
@@ -49,25 +51,25 @@ font = {'family' : 'Bitstream Vera Sans',
 
 plt.rc('font', **font)
 
-'''
+
 plt.plot(data2[:,0], data2[:,1], color='black', label='wB97X/6-31G*',linewidth=2)
 plt.scatter(data2[:,0], data2[:,1], color='black',linewidth=4)
-plt.plot(data3[:,0], data3[:,1],'r--', color='blue', label='ANN - GDB-4 RMSE: ' + "{:.6f}".format(rmse4) + "Ha/atom",linewidth=2)
-plt.scatter(data3[:,0], data3[:,1], color='blue',linewidth=4)#
 plt.plot(data4[:,0], data4[:,1],'r--', color='red', label='ANN - GDB-5 RMSE: ' + "{:.6f}".format(rmse5) + "Ha/atom",linewidth=2)
 plt.scatter(data4[:,0], data4[:,1], color='red',linewidth=4)
-plt.plot(data5[:,0], data5[:,1],'r--', color='green', label='ANN - GDB-6 RMSE: ' + "{:.6f}".format(rmse6) + "Ha/atom",linewidth=2)
+plt.plot(data5[:,0], data5[:,1],'r--', color='green', label='ANN - GDB-62 RMSE: ' + "{:.6f}".format(rmse61) + "Ha/atom",linewidth=2)
 plt.scatter(data5[:,0], data5[:,1], color='green',linewidth=4)
-plt.plot(data6[:,0], data6[:,1],'r--', color='orange', label='ANN - GDB-6AA RMSE: ' + "{:.6f}".format(rmse6AA) + "Ha/atom",linewidth=2)
+plt.plot(data6[:,0], data6[:,1],'r--', color='orange', label='ANN - GDB-62 RMSE: ' + "{:.6f}".format(rmse62) + "Ha/atom",linewidth=2)
 plt.scatter(data6[:,0], data6[:,1], color='orange',linewidth=4)
-'''
+plt.plot(data6[:,0], data6[:,1],'r--', color='blue', label='ANN - GDB-6AA RMSE: ' + "{:.6f}".format(rmse6AA) + "Ha/atom",linewidth=2)
+plt.scatter(data6[:,0], data6[:,1], color='blue',linewidth=4)
 
+'''
 plt.plot(data2[:,1], data2[:,1], color='black', label='wB97X/6-31G*',linewidth=2)
 plt.scatter(data2[:,1], data3[:,1], color='blue', label='ANN - GDB-4 RMSE: ' + "{:.6f}".format(rmse4) + "Ha/atom",linewidth=2)
 plt.scatter(data2[:,1], data4[:,1], color='red', label='ANN - GDB-5 RMSE: ' + "{:.6f}".format(rmse5) + "Ha/atom",linewidth=2)
 plt.scatter(data2[:,1], data5[:,1], color='green', label='ANN - GDB-6 RMSE: ' + "{:.6f}".format(rmse6) + "Ha/atom",linewidth=2)
 plt.scatter(data2[:,1], data6[:,1], color='orange', label='ANN - GDB-6AA RMSE: ' + "{:.6f}".format(rmse6AA) + "Ha/atom",linewidth=2)
-
+'''
 
 plt.title("Energy Differences Between 50 Random Structures\nPolypeptide Chain: H-Gly-Pro-Hyp-Gly-Ala-Gly-OH")
 #plt.xlabel('Conformation Pair (Count 49)')
--- HD-AtomNNP/RMSEPlot.py
+++ HD-AtomNNP/RMSEPlot.py
@@ -9,22 +9,22 @@ import matplotlib as mpl
 import random
 import graphtools as gt
 
+def graphRMSEperATOM(dir, plot, label, color='black'):
+    user = os.environ['USER']
+    file = 'RMSEperATOM.dat'
+    data1 = gt.getfltsfromfile('/home/' + user + dir + file, [0])
+    data2 = gt.getfltsfromfile('/home/' + user + dir + file, [1])
+
+    plot.scatter(data1, data2, color=color, label=label,linewidth=2)
+    plot.plot(data1, data2, color=color, label=label,linewidth=2)
+
+
+
 # -----------------------
 cmap = mpl.cm.brg
 # ------------
 # AM1 vs Act
 # ------------
-user = os.environ['USER']
-
-dir = '/Research/ANN-Test-Data/GDB-11-W98XD-6-31gd/train_05/'
-dir = '/Research/ANN-Test-Data/GDB-11-W98XD-6-31gd/train_06/'
-file = 'RMSEperATOM.dat'
-
-data1 = gt.getfltsfromfile('/home/' + user + dir + file, [0])
-data2 = gt.getfltsfromfile('/home/' + user + dir + file, [1])
-data3 = gt.getfltsfromfile('/home/' + user + dir2 + file, [1])
-
-print('Datasize: ' + str(data1.shape[0]))
 
 font = {'family' : 'Bitstream Vera Sans',
             'weight' : 'normal',
@@ -32,8 +32,10 @@ font = {'family' : 'Bitstream Vera Sans',
 
 plt.rc('font', **font)
 
-plt.scatter(data1, data2, color='blue', label='ANN - GDB3',linewidth=2)
-plt.plot(data1, data2, color='blue', label='ANN - GDB3',linewidth=2)
+graphRMSEperATOM('/Research/ANN-Test-Data/GDB-11-W98XD-6-31gd/train_05/',plt,'ANN - GDB5','black')
+graphRMSEperATOM('/Research/ANN-Test-Data/GDB-11-W98XD-6-31gd/train_06/',plt,'ANN - GDB6','blue')
+graphRMSEperATOM('/Research/ANN-Test-Data/GDB-11-W98XD-6-31gd/train_06_2/',plt,'ANN - GDB62','red')
+graphRMSEperATOM('/Research/ANN-Test-Data/GDB-11-W98XD-6-31gd/train_06AA/',plt,'ANN - GDB6AA','green')
 
 plt.title("RMSE per Atom for each dataset")
 plt.xlabel('File index')
