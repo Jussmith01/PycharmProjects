0c22f068b7569ce4a51e4c64b4f56a3b50bea44c
Justin <jussmith48@gmail.com>
2016-08-08 10:20:34 -0400

    Update

--- HD-AtomNNP/histograph.py
+++ HD-AtomNNP/histograph.py
@@ -9,11 +9,11 @@ import matplotlib as mpl
 import random
 import graphtools as gt
 
-def histograph(dir, file, bins, label, color='black'):
+def histograph(dir, file, bins, norm, label, color='black'):
     user = os.environ['USER']
     data = gt.getfltsfromfile('/home/' + user + dir + file,' ', [0])
 
-    plt.hist(data, bins, color=color,normed=0, label=label,linewidth=2)
+    plt.hist(data, bins, color=color,normed=norm, label=label,linewidth=2)
 
 
 # -----------------------
@@ -29,9 +29,14 @@ font = {'family' : 'Bitstream Vera Sans',
 
 plt.rc('font', **font)
 
-histograph('/Research/ANN-Test-Data/GDB-11-W98XD-6-31gd/dnntsgdb11_06/','output.dat',50,'','blue')
-histograph('/Research/ANN-Test-Data/GDB-11-W98XD-6-31gd/dnntsgdb11_05/','output.dat',50,'','red')
-histograph('/Research/ANN-Test-Data/GDB-11-W98XD-6-31gd/dnntsgdb11_04/','output.dat',50,'','green')
+print ('Plotting 07')
+histograph('/Research/GDB-11-W98XD-6-31gd/dnntsgdb11_07/','output.dat',200,0,'','orange')
+print ('Plotting 06')
+histograph('/Research/GDB-11-W98XD-6-31gd/dnntsgdb11_06/','output.dat',100,0,'','blue')
+print ('Plotting 05')
+histograph('/Research/GDB-11-W98XD-6-31gd/dnntsgdb11_05/','output.dat',100,0,'','red')
+print ('Plotting 04')
+histograph('/Research/GDB-11-W98XD-6-31gd/dnntsgdb11_04/','output.dat',100,0,'','green')
 
 plt.title("Energy Differences Between 50 Random Structures\nPolypeptide Chain: H-Gly-Pro-Hyp-Gly-Ala-Gly-OH")
 #plt.xlabel('Conformation Pair (Count 49)')
