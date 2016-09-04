822cd467a422289fe71a1f98c148af61b291e3e4
jujuman <jsmith48@ufl.edu>
2016-09-04 10:03:14 -0400

    pyneurochem testing

--- HD-AtomNNP/HDAtomTesting.py
+++ HD-AtomNNP/HDAtomTesting.py
@@ -1,13 +1,33 @@
 __author__ = 'jujuman'
 
-import libpyNeuroChem as nc
+# Import pyNeuroChem
+import pyNeuroChem as pync
 
-cnstfile = '/home/jujuman/Research/trainingcases/wB97X-631gd-train-highgarden/train_08-a3.1A_r4.5_dn2/rHCNO-4.5A_32-3.1A_a8-8.params'
-saefile = '/home/jujuman/Research/trainingcases/wB97X-631gd-train-highgarden/sae_6-31gd.dat'
-nnfdir = '/home/jujuman/Research/trainingcases/wB97X-631gd-train-highgarden/train_08-a3.1A_r4.5_dn2/networks/'
+# Set required files for pyNeuroChem
+wkdir    = '/home/jujuman/Research/trainingcases/wB97X-631gd-train-highgarden/'
+cnstfile = wkdir + 'nw-64-64-64-32/train_07-a3.1A_r4.5/rHCNO-4.5A_32-3.1A_a8-8.params'
+saefile  = wkdir + 'sae_6-31gd.dat'
+nnfdir   = wkdir + 'nw-64-64-64-32/train_07-a3.1A_r4.5/networks/'
 
-test = nc.pyNeuroChem(cnstfile,saefile,nnfdir,0)
+# Construct pyNeuroChem class
+nc = pync.pyNeuroChem(cnstfile,saefile,nnfdir,1)
 
-list = [1.0,2.0,3.0]
+# List of atom types of Molecule (H2O)
+typ = ['C','H','H','H','H']
 
-test.testfunction(list,3)
+# Save list 4 conformers of H2O
+xyz = [[0.000000, 0.000000, 0.00000, 0.6418, 0.6418, 0.6418,-0.6418,-0.6418,0.6418,-0.6418,0.6418,-0.6418,0.6418,-0.6418,-0.6418]]
+
+# Set the conformers in NeuroChem
+nc.setConformers(confs=xyz,types=typ)
+
+# Print some data from the NeuroChem
+print( 'Number of Atoms Loaded: ' + str(nc.getNumAtoms()) )
+print( 'Number of Confs Loaded: ' + str(nc.getNumConfs()) )
+
+# Compute Forces of Conformations
+F = nc.optimizeGeom(0)
+
+for i in F:
+    for j in range(0,5):
+        print (typ[j] + ' ' + "{:.7f}".format(i[j*3+0]) + ' ' + "{:.7f}".format(i[j*3+1]) + ' ' + "{:.7f}".format(i[j*3+2]))
