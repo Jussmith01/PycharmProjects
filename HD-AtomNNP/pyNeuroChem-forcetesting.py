aa85149be3a7ca6881396e4a69cd81f26e4818c1
Jujuman <jsmith48@ufl.edu>
2016-11-22 09:19:55 -0500

    Update optimizer

--- HD-AtomNNP/pyNeuroChem-forcetesting.py
+++ HD-AtomNNP/pyNeuroChem-forcetesting.py
@@ -3,26 +3,30 @@ __author__ = 'jujuman'
 # Import pyNeuroChem
 import pyNeuroChem as pync
 import numpy as np
+import graphtools as gt
+
 
 # Set required files for pyNeuroChem
-wkdir    = '/home/jujuman/Dropbox/Research/NeuroChemForceTesting/train_02/'
-cnstfile = wkdir + 'rHO-3.0A_4-2.5A_a2-2.params'
-saefile  = wkdir + '../sae_6-31gd.dat'
+wkdir    = '/home/jujuman/Dropbox/ChemSciencePaper.AER/ANI-1-ntwk/'
+cnstfile = wkdir + 'rHCNO-4.6A_32-3.1A_a8-8.params'
+saefile  = wkdir + 'sae_6-31gd.dat'
 nnfdir   = wkdir + 'networks/'
 
 xyz = [[0.00000,0.00000,0.37124,0.00000, 0.00000,-0.37124]
 	  ,[0.00000,0.372635,0.00000,0.00000,-0.372635, 0.00000]]
 	  #,[0.00000,0.00000,0.41000,0.00000, 0.00000,-0.41000]]
 
-xyz = [[0.0, 0.0, 0.118604, 0.0, 0.67007291, -0.40437055, 0.0, -0.75981897, -0.474415]]#, [0.0, 0.0, 0.118604, 0.0, 0.67086124, -0.40498585, 0.0, -0.75981897, -0.474415]]
+xyz = [[0.0, 0.0, 0.118604, 0.0, 0.67007291, -0.40437055, 0.0, -0.75981897, -0.474415], [0.0, 0.0, 0.118604, 0.0, 0.67007291, -0.4043705, 0.0, -0.75981897, -0.474415]]
 
 typ = ['O','H','H']
+
+xyz,typ,Na = gt.readxyz('/home/jujuman/Dropbox/ChemSciencePaper.AER/TestCases/Retinol/optimization/opt_test_numeric.xyz')
 
 # Construct pyNeuroChem class
 nc = pync.pyNeuroChem(cnstfile, saefile, nnfdir, 0)
 
 # Set the conformers in NeuroChem
-nc.setConformers(confs=xyz,types=typ)
+nc.setConformers(confs=xyz,types=typ[0])
 
 # Print some data from the NeuroChem
 print( 'Number of Atoms Loaded: ' + str(nc.getNumAtoms()) )
