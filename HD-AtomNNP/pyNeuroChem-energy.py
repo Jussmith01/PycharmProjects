c11f8436733b586cb6bd838a73d0f1a06fc5557e
Jujuman <jsmith48@ufl.edu>
2016-12-04 10:51:42 -0500

    Update

--- HD-AtomNNP/pyNeuroChem-energy.py
+++ HD-AtomNNP/pyNeuroChem-energy.py
@@ -15,6 +15,10 @@ xyz = np.array([[[0.00000000,  0.07775414,  0.14191920],  [0.00000000,  0.875117
 	  		   ,[[0.00000000,  0.07782031,  0.14223997],  [0.00000000,  0.87485045, -0.40213168],  [0.00000000, -0.64241695, -0.50028986]]
 	  		   ,[[0.00000000,  0.07782238,  0.14223947],  [0.00000000,  0.87484127, -0.40213102],  [0.00000000, -0.64240986, -0.50029004]]
 	  		   ,[[0.00000000,  0.07782238,  0.14223947],  [0.00000000,  0.87484127, -0.40213102],  [0.00000000, -0.64240986, -0.50029004]]],dtype=np.float32)
+
+xyz2 = np.array([[0.00000000,  0.2775414,  0.14191920],  [0.00000000,  0.87511754, -0.40200517],  [0.00000000, -0.64261788, -0.50009561]],dtype=np.float32)
+
+
 typ = ['O','H','H']
 Na  = [3]
 
@@ -26,13 +30,18 @@ print (xyz)
 nc = pync.pyNeuroChem(cnstfile, saefile, nnfdir, 0)
 
 # Set the conformers in NeuroChem
-nc.setConformers(confs=xyz,types=typ)
+#nc.setConformers(confs=xyz2,types=typ)
+nc.setMolecule(coords=xyz2,types=typ)
 
 # Print some data from the NeuroChem
 print( 'Number of Atoms Loaded: ' + str(nc.getNumAtoms()) )
 print( 'Number of Confs Loaded: ' + str(nc.getNumConfs()) )
 
 # Compute Energies of Conformations
-E = np.array( nc.computeEnergies() )
+E = nc.energy()
 
 print(E)
+
+F = nc.force()
+
+print(F)
