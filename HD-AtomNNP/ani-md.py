24a2d5083894e0356092a6a4148e10d42cfafdbc
Justin <jsmith48@ufl.edu>
2017-01-23 08:07:26 -0500

    MD update

--- HD-AtomNNP/ani-md.py
+++ HD-AtomNNP/ani-md.py
@@ -13,6 +13,9 @@ from ase.md.langevin import Langevin
 from ase.io.trajectory import Trajectory
 from ase import units
 
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+
 from ase.optimize.fire import FIRE as QuasiNewton
 
 from ase.md.nvtberendsen import NVTBerendsen
@@ -37,20 +40,21 @@ saefile  = anipath + '/sae_6-31gd.dat'
 nnfdir   = anipath + '/networks/'
 
 # Construct pyNeuroChem class
-nc = pync.molecule(cnstfile, saefile, nnfdir, 0)
+nc = pync.molecule(cnstfile, saefile, nnfdir, 1)
 
-bz = read('/home/jujuman/Dropbox/ChemSciencePaper.AER/Poster-GTC-May-2017/Timings/min1.xyz')
+bz = read('/home/jujuman/Dropbox/ChemSciencePaper.AER/Poster-GTC-May-2017/Timings/benzene.xyz')
+#bz = read('/home/jujuman/Research/GDB-11-wB97X-6-31gd/dnnts_begdb/begdb-h2oclusters/xyz/4197_water6CB2.xyz')
 
-#L = 16.7
+#L = 25.0
 #bz.set_cell(([[L,0,0],[0,L,0],[0,0,L]]))
 #bz.set_pbc((True, True, True))
 
 bz.set_calculator(NeuroChem2ASE(nc))
 
-start_time = time.time()
-dyn = LBFGS(bz)
-dyn.run(fmax=0.001)
-print('[ANI Total time:', time.time() - start_time, 'seconds]')
+#start_time = time.time()
+#dyn = LBFGS(bz)
+#dyn.run(fmax=0.001)
+#print('[ANI Total time:', time.time() - start_time, 'seconds]')
 
 # Write visualization of molecule
 f = open("optmol.xyz",'w')
@@ -69,32 +73,57 @@ dyn = Langevin(bz, 0.25 * units.fs, T * units.kB, 0.05)
 
 start_loop_time = time.time()
 
+distCH = []
+distCC = []
+
 mdcrd = open("mdcrd.xyz",'w')
 temp = open("temp.dat",'w')
-def printenergy(a=bz,b=mdcrd,d=dyn,t=temp,loop_tm=start_loop_time):  # store a reference to atoms in the
+def printenergy(a=bz,b=mdcrd,d=dyn,t=temp,disCH=distCH,disCC=distCC):  # store a reference to atoms in the
     """Function to print the potential, kinetic and total energy."""
     epot = a.get_potential_energy() / len(a)
     ekin = a.get_kinetic_energy() / len(a)
     print('Step %i - Energy per atom: Epot = %.3feV  Ekin = %.3feV (T=%3.0fK)  '
-          'Etot = %.3feV Time: %.3fs' % (d.get_number_of_steps(),epot, ekin, ekin / (1.5 * units.kB), epot + ekin, time.time() - loop_tm))
+          'Etot = %.3feV Time: %.3fs' % (d.get_number_of_steps(),epot, ekin, ekin / (1.5 * units.kB), epot + ekin, time.time()))
 
-    loop_tm = time.time()
+    disCH.append(np.sqrt(np.power(a[0].x-a[6].x,2)+np.power(a[0].y-a[6].y,2)+np.power(a[0].z-a[6].z,2)))
+    disCC.append(np.sqrt(np.power(a[0].x-a[5].x,2)+np.power(a[0].y-a[5].y,2)+np.power(a[0].z-a[5].z,2)))
 
     t.write(str(d.get_number_of_steps()) + ' ' + str(ekin / (1.5 * units.kB)) + ' ' + str(epot) + ' ' +  str(ekin) + ' ' + str(epot + ekin) + '\n')
     b.write('\n' + str(len(a)) + '\n')
     for i in a:
         b.write(str(i.symbol) + ' ' + str(i.x) + ' ' + str(i.y) + ' ' + str(i.z) + '\n')
 
-dyn.attach(printenergy, interval=5)
+dyn.attach(printenergy, interval=1)
 #dyn.attach(MDLogger(dyn, bz, 'bz_md_NVT_10ps_1fs.log', header=True, stress=False,
 #           peratom=False, mode="w"), interval=50)
 
-printenergy()
-print("test")
+#printenergy()
 
 start_time = time.time()
-dyn.run(4*1000*1000) # Do 1ns of MD
+dyn.run(2**20) # Do 1ns of MD
 print('[ANI Total time:', time.time() - start_time, 'seconds]')
+
+distCH = np.array(distCH)
+distCH = distCH - distCH.mean()
+dfftCH = np.abs(np.fft.rfft(distCH))/float(len(distCC))
+freqCH = np.fft.rfftfreq(distCH.shape[-1])
+
+distCC = np.array(distCC)
+distCC = distCC - distCC.mean()
+dfftCC = np.abs(np.fft.rfft(distCC))/float(len(distCC))
+freqCC = np.fft.rfftfreq(distCC.shape[-1])
+
+print (freqCC)
+
+font = {'family' : 'Bitstream Vera Sans',
+        'weight' : 'normal',
+        'size'   : 9}
+
+plt.rc('font', **font)
+
+plt.plot(freqCH, dfftCH, freqCC, dfftCC)
+
+plt.show()
 
 mdcrd.close()
 temp.close()
