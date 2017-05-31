5724006bfb192574f38f26cee8ba1117506b3b31
Justin <jsmith48@ufl.edu>
2017-05-30 20:36:39 -0400

    Update

--- CV_data_generator/cross_validate_cachecreator.py
+++ CV_data_generator/cross_validate_cachecreator.py
@@ -2,14 +2,13 @@ import numpy as np
 import pyanitools as pyt
 from pyNeuroChem import cachegenerator as cg
 
-wkdir = '/home/jujuman/Research/ANI-DATASET'
+wkdir = '/home/jujuman/Research/DataReductionMethods/models/cv'
 
-saef   = "/home/jujuman/Research/ANI-DATASET/ANI-G09-DIV/sae_6-31gd.dat"
+saef   = wkdir + "/sae_6-31gd.dat"
+print(saef)
+h5files = ["/home/jujuman/Research/DataReductionMethods/models/train_c08f/ani_red_c08f.h5"]
 
-h5files = [wkdir + "/h5data/gdb9-2500-div_new.h5",
-           wkdir + "/h5data/ani-gdb-c08e.h5",]
-
-store_dir = wkdir + "/ANI-G09-DIV/cache-c08e-"
+store_dir = wkdir + "/cache-c08e-"
 
 #adl.split_load(10)
 N = 10
@@ -21,15 +20,15 @@ train_idx = [[2, 3, 4, 5, 6, 7, 8, 9],
              [0, 1, 2, 3, 4, 5, 6, 7]
              ]
 
-valid_idx = [[0]
-            ,[2]
-            ,[4]
-            ,[6]
-            ,[8]
+valid_idx = [[0, 1]
+            ,[2, 3]
+            ,[4, 5]
+            ,[6, 7]
+            ,[8, 9]
              ]
 
-cachet = [cg('_train', saef, store_dir + str(r) + '/') for r in range(5)]
-cachev = [cg('_valid', saef, store_dir + str(r) + '/') for r in range(5)]
+cachet = [cg('_train', saef, store_dir + str(r) + '/', False) for r in range(5)]
+cachev = [cg('_valid', saef, store_dir + str(r) + '/', False) for r in range(5)]
 
 for fn in h5files:
     adl = pyt.anidataloader(fn)
--- HD-AtomNNP/HDAtomTraining_scripts/create_train_cache_from_h5.py
+++ HD-AtomNNP/HDAtomTraining_scripts/create_train_cache_from_h5.py
@@ -5,20 +5,23 @@ import pyanitools as pyt
 from pyNeuroChem import cachegenerator as cg
 
 # Set the HDF5 file containing the data
-hdf5files = [#'/home/jujuman/Research/ANI-DATASET/RXN1_TNET/rxn_h5_data/ani_benz_rxn_test_1.h5',
-             #'/home/jujuman/Research/ANI-DATASET/RXN1_TNET/rxn_h5_data/ani_benz_rxn_test_2.h5',
-             #'/home/jujuman/Research/ANI-DATASET/RXN1_TNET/rxn_h5_data/ani_benz_rxn_test_3.h5',
-             #'/home/jujuman/Research/ANI-DATASET/RXN1_TNET/rxn_h5_data/ani_benz_rxn_test_4.h5',
-             #'/home/jujuman/Research/ANI-DATASET/RXN1_TNET/rxn_h5_data/ani_benz_rxn_test_5.h5',
-             #'/home/jujuman/Research/ANI-DATASET/RXN1_TNET/rxn_h5_data/ani_benz_rxn_test_6.h5',
+hdf5files = [('/home/jujuman/Research/ANI-DATASET/RXN1_TNET/rxn_h5_data/ani_benz_rxn_test_1.h5','dbmrxn1'),
+             ('/home/jujuman/Research/ANI-DATASET/RXN1_TNET/rxn_h5_data/ani_benz_rxn_test_2.h5','dbmrxn2'),
+             ('/home/jujuman/Research/ANI-DATASET/RXN1_TNET/rxn_h5_data/ani_benz_rxn_test_3.h5','dbmrxn3'),
+             ('/home/jujuman/Research/ANI-DATASET/RXN1_TNET/rxn_h5_data/ani_benz_rxn_test_4.h5','dbmrxn4'),
+             ('/home/jujuman/Research/ANI-DATASET/RXN1_TNET/rxn_h5_data/ani_benz_rxn_test_5.h5','dbmrxn5'),
+             ('/home/jujuman/Research/ANI-DATASET/RXN1_TNET/rxn_h5_data/ani_benz_rxn_test_6.h5','dbmrxn6'),
              #'/home/jujuman/Research/ANI-DATASET/h5data/ani-gdb-c08f.h5',
-             '/home/jujuman/Research/ANI-DATASET/h5data/ani-gdb-c08f.h5',
-             ]
+	     ('/home/jujuman/Research/ANI-DATASET/h5data/gdb9-2500-bad_new.h5','gdb9-2500-bad'),
+	     ('/home/jujuman/Research/ANI-DATASET/h5data/gdb9-2500-div_new.h5','gdb9-2500-div'),
+	     ('/home/jujuman/Research/ANI-DATASET/h5data/gdb9-2500-div-dim_35.h5','gdb9-2500-div-dim'),
+	     ('/home/jujuman/Research/ANI-DATASET/h5data/sf_new.h5','sf_data'),
+             ('/home/jujuman/Research/ANI-DATASET/h5data/ani-gdb-c08f.h5','gdbc08f'),]
 
 #hdf5file = '/home/jujuman/Research/ANI-DATASET/ani-1_data_c03.h5'
-storecac = '/home/jujuman/Research/SingleNetworkTest/cache08f/'
-saef   = "/home/jujuman/Research/SingleNetworkTest/sae_6-31gd.dat"
-path = "/home/jujuman/Research/SingleNetworkTest/cache08f/testset/testset.h5"
+storecac = '/home/jujuman/Research/SingleNetworkTest/cachec08f09augdbm6/'
+saef   = "/home/jujuman/Research/SingleNetworkTest/sae_wb97x-631gd_HCNOFS.dat"
+path = "/home/jujuman/Research/SingleNetworkTest/cachec08f09augdbm6/testset/testset.h5"
 
 
 # Declare data cache
@@ -31,7 +34,7 @@ dpack = pyt.datapacker(path)
 for f in hdf5files:
     # Construct the data loader class
     print(f)
-    adl = pyt.anidataloader(f)
+    adl = pyt.anidataloader(f[0])
 
     print(adl.get_group_list())
 
@@ -51,11 +54,13 @@ for f in hdf5files:
         cachet.insertdata(np.concatenate(xyz[0:8]), np.array(np.concatenate(eng[0:8]), dtype=np.float64), list(spc))
         cachev.insertdata(xyz[8], np.array(eng[8], dtype=np.float64), list(spc))
 
+        Na = len(list(spc))
+
         # Prepare and store the test data set
         if xyz[9].shape[0] != 0:
             #print(xyz[9].shape)
-            t_xyz = xyz[9].reshape(xyz[9].shape[0],xyz[9].shape[1]*xyz[9].shape[2])
-            dpack.store_data(nme + '/mol' + str(i), coordinates=t_xyz, energies=np.array(eng[9]), species=spc)
+            t_xyz = xyz[9].reshape(xyz[9].shape[0], Na*3)
+            dpack.store_data(f[1] + '/mol' + str(i), coordinates=t_xyz, energies=np.array(eng[9]), species=spc)
     print('Count: ',dc)
 
     adl.cleanup()
